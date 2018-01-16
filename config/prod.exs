dc7c1f1b1d14981c31f3c260a45211366f41b3e4
Daniel Cunha <daniel.cunha@pipefy.com>
2018-01-16 20:36:33 -0200

    Fixing Repo prod config

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :cwb_graphql_intro, CwbGraphqlIntroWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-config :hello, Hello.Repo,
+config :cwb_graphql_intro, CwbGraphqlIntro.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
